c587a47711b3fb843be0dff369389cd67d37c0c0
jweiteka <jweite@yahoo.com>
2015-08-18 17:03:12 -0400

    First inheritance from logstash-input-file

--- lib/logstash/inputs/csvfile.rb
+++ lib/logstash/inputs/csvfile.rb
@@ -1,38 +1,13 @@
 # encoding: utf-8
-require "logstash/inputs/base"
+require "logstash/inputs/file"
 require "logstash/namespace"
-require "stud/interval"
-require "socket" # for Socket.gethostname
 
-# Generate a repeating message.
-#
-# This plugin is intented only as an example.
+class LogStash::Inputs::CSVFile < LogStash::Inputs::File
+	config_name "csvfile"
 
-class LogStash::Inputs::CSVFile < LogStash::Inputs::Base
-  config_name "csvfile"
+	def decorate(event)
+		super(event)
+		event["_csv"] = true
+	end
 
-  # If undefined, Logstash will complain, even if codec is unused.
-  default :codec, "plain" 
-
-  # The message string to use in the event.
-  config :message, :validate => :string, :default => "Hello World!"
-
-  # Set how frequently messages should be sent.
-  #
-  # The default, `1`, means send a message every second.
-  config :interval, :validate => :number, :default => 1
-
-  public
-  def register
-    @host = Socket.gethostname
-  end # def register
-
-  def run(queue)
-    Stud.interval(@interval) do
-      event = LogStash::Event.new("message" => @message, "host" => @host)
-      decorate(event)
-      queue << event
-    end # loop
-  end # def run
-
-end # class LogStash::Inputs::Example
+end # class LogStash::Inputs::CSVFile
--- logstash-input-csvfile.gemspec
+++ logstash-input-csvfile.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name = 'logstash-input-csvfile'
-  s.version         = '0.0.1'
+  s.version         = '0.0.2'
   s.licenses = ['Apache License (2.0)']
   s.summary = "This example input streams a string at a definable interval."
   s.description = "This gem is a logstash plugin required to be installed on top of the Logstash core pipeline using $LS_HOME/bin/plugin install gemname. This gem is not a stand-alone program"
@@ -20,6 +20,7 @@ Gem::Specification.new do |s|
   # Gem dependencies
   s.add_runtime_dependency "logstash-core", '>= 1.4.0', '< 2.0.0'
   s.add_runtime_dependency 'logstash-codec-plain'
+  s.add_runtime_dependency 'logstash-input-file'
   s.add_runtime_dependency 'stud'
   s.add_development_dependency 'logstash-devutils'
 end
